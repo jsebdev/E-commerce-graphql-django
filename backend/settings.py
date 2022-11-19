9827b08614ed670451ad44cac467a1311dd0d1f2
Sebastian Caicedo <jsebdev@gmail.com>
2022-11-19 13:32:48 +0100

    Containerzation

--- backend/settings.py
+++ backend/settings.py
@@ -85,10 +85,9 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'NAME': BASE_DIR / 'database' / 'db.sqlite3',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.1/ref/settings/#auth-password-validators
