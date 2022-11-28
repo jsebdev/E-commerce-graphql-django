15ea299951747bf82a26f26e820af43b19cc15ac
Sebastian Caicedo <jsebdev@gmail.com>
2022-11-28 21:05:26 +0100

    remove password constraints

--- backend/settings.py
+++ backend/settings.py
@@ -219,3 +219,21 @@ MEDIA_URL = '/media/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, DATA_DIR, 'static')
 STATIC_URL = '/static/'
+
+AUTH_PASSWORD_VALIDATORS = [
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
+    # },
+    {
+        'NAME': 'django.contrib.auth.password_validation.MinimumLengthValidator',
+        'OPTIONS': {
+            'min_length': 8,
+        }
+    },
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.CommonPasswordValidator',
+    # },
+    # {
+    #     'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
+    # },
+]
